7aa2b6f5f92dcb44579d63e60ccf80cdb1df6a25
sangueheroi <sangue.heroi@gmail.com>
2016-05-22 19:31:12 -0300

    Commit - 22/05/2016 - 19h30 - Daniel

---
+++ SangueHeroiWeb/SangueHeroiWeb/Controllers/RankingDoadoresController.cs
@@ -0,0 +1,27 @@
+﻿using SangueHeroiWeb.DAO;
+using SangueHeroiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SangueHeroiWeb.Controllers
+{
+    public class RankingDoadoresController : Controller
+    {
+        // GET: Ranking de Doadores
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetListRankingDoadores()
+        {
+            DoacaoDAO dd = new DoacaoDAO();
+            List<DoacaoModel> list = dd.getRankingDoadores();
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }    
+    }
+}
