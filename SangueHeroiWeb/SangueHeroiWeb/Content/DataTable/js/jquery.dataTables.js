64b2ac9733a69586728652c555a19c57347ae7f5
sangueheroi <sangue.heroi@gmail.com>
2016-07-30 12:26:00 -0300

    Commit - 30/07/2016 - 12:24h - Yuri

--- SangueHeroiWeb/SangueHeroiWeb/Content/DataTable/js/jquery.dataTables.js
+++ SangueHeroiWeb/SangueHeroiWeb/Content/DataTable/js/jquery.dataTables.js
@@ -2842,7 +2842,7 @@
 		var language = settings.oLanguage;
 		var previousSearch = settings.oPreviousSearch;
 		var features = settings.aanFeatures;
-		var input = '<input type="search" class="'+classes.sFilterInput+'"/>';
+		var input = '<input type="search" class="form-control-custom ' + classes.sFilterInput + '"/>';
 	
 		var str = language.sSearch;
 		str = str.match(/_INPUT_/) ?
